60eacbc9ccaab08ef39533cac31c3ba977c29dc4
tnaton <tnaton@student.42.fr>
2022-10-08 02:08:37 +0200

    pfiou ptit dodo la

--- iterator_traits.hpp
+++ iterator_traits.hpp
@@ -15,6 +15,17 @@
 # include <iterator>
 
 namespace ft {
+
+	struct random_access_iterator_tag {};
+
+	struct bidirectional_iterator_tag {};
+
+	struct forward_iterator_tag {};
+
+	struct input_iterator_tag {};
+
+	struct output_iterator_tag {};
+
 	template<class Iter> struct iterator_traits {
 		typedef typename Iter::difference_type		difference_type;
 		typedef typename Iter::value_type			value_type;
@@ -24,12 +35,64 @@ namespace ft {
 	};
 
 	template<class T> struct iterator_traits<T*> {
-		typedef typename std::ptrdiff_t						difference_type;
-		typedef T									value_type;
-		typedef T*									pointer;
-		typedef T&									reference;
-		typedef typename std::random_access_iterator_tag	iterator_category;
+		typedef ptrdiff_t									difference_type;
+		typedef T											value_type;
+		typedef T*											pointer;
+		typedef T&											reference;
+		typedef typename ft::random_access_iterator_tag		iterator_category;
 	};
+
+	template<class T> struct iterator_traits<const T*> {
+		typedef ptrdiff_t									difference_type;
+		typedef T											value_type;
+		typedef T*											pointer;
+		typedef T&											reference;
+		typedef typename ft::random_access_iterator_tag		iterator_category;
+	};
+
+	template<class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
+		class iterator {
+			typedef Distance	difference_type;
+			typedef T			value_type;
+			typedef Pointer		pointer;
+			typedef Reference	reference;
+			typedef Category	iterator_category;
+		};
+
+	template<class T>
+		class InputIterator : ft::iterator<ft::input_iterator_tag, T> {
+			typedef typename ft::iterator<ft::input_iterator_tag, T>::difference_type	difference_type;
+			typedef typename ft::iterator<ft::input_iterator_tag, T>::value_type		value_type;
+			typedef typename ft::iterator<ft::input_iterator_tag, T>::pointer			pointer;
+			typedef typename ft::iterator<ft::input_iterator_tag, T>::reference			reference;
+			typedef typename ft::iterator<ft::input_iterator_tag, T>::iterator_category	iterator_category;
+
+			InputIterator(pointer ptr): _ptr(ptr) {}
+
+			InputIterator(const InputIterator & other): _ptr(other._ptr) {}
+
+			reference	operator*(void) const {
+				return (*_ptr);
+			}
+
+			pointer		operator->(void) const {
+				return (_ptr);
+			}
+
+			reference	operator++(void) {
+				_ptr++;
+				return (*this);
+			}
+
+			InputIterator	operator++(int) {
+				InputIterator tmp(*this);
+				_ptr++;
+				return tmp;
+			}
+
+			protected:
+				pointer	_ptr;
+		};
 }
 #endif
 
--- vector.hpp
+++ vector.hpp
@@ -15,56 +15,9 @@
 # include "iterator.hpp"
 # include <memory>
 # include <iostream>
+# include "utils.hpp"
 
 namespace ft {
-
-	template<bool, class T = void>
-	struct	enable_if {};
-
-	template<class T>
-	struct	enable_if<true, T> { typedef T type; };
-
-	template<bool is_integral, typename T>
-	struct	is_integral_test {
-		typedef T type;
-		static const bool value = is_integral;
-	};
-
-	template <typename>
-		struct is_integral_type : is_integral_test<false, bool> {};
-
-	template <>
-		struct is_integral_type<bool> : is_integral_test<true, bool> {};
-
-	template <>
-		struct is_integral_type<char> : is_integral_test<true, char> {};
-	
-	template <>
-		struct is_integral_type<signed char> : is_integral_test<true, signed char> {};
-	
-	template <>
-		struct is_integral_type<unsigned char> : is_integral_test<true, unsigned char> {};
-	
-	template <>
-		struct is_integral_type<short int> : is_integral_test<true, short int> {};
-	
-	template <>
-		struct is_integral_type<unsigned short int> : is_integral_test<true, unsigned short int> {};
-	
-	template <>
-		struct is_integral_type<int> : is_integral_test<true, int> {};
-	
-	template <>
-		struct is_integral_type<unsigned int> : is_integral_test<true, unsigned int> {};
-	
-	template <>
-		struct is_integral_type<long int> : is_integral_test<true, long int> {};
-	
-	template <>
-		struct is_integral_type<unsigned long int> : is_integral_test<true, unsigned long int> {};
-
-	template <typename T>
-		struct is_integral : is_integral_type<T> {};
 
 	template <class T, class Allocator = std::allocator<T> > class vector {
 		// definition des types
