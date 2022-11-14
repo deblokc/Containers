ec09ce3a8d85fca096a5714cbb9d2dde54635602
Theodore NATON <tnaton@e3r11p13.clusters.42paris.fr>
2022-11-14 12:35:06 +0100

    suu

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 19:09:37 by tnaton            #+#    #+#             */
-/*   Updated: 2022/11/14 11:11:29 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/11/14 12:34:42 by tnaton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //#include "vector.hpp"
@@ -21,6 +21,7 @@ class	Clock {
 	public:
 		Clock(void) {gettimeofday(&_time, NULL);};
 		long time(void) {struct timeval now; gettimeofday(&now, NULL); return (((now.tv_sec * 1000) - (_time.tv_sec * 1000)) + (now.tv_usec / 1000) - (_time.tv_usec / 1000));}
+		void p(void) {long tmp = time(); std::cout << tmp/1000 << "." << tmp%1000 << "s" << std::endl;}
 	private:
 		struct timeval _time;
 };
@@ -53,10 +54,17 @@ void print(int id, const ft::vector<int>& container)
 */
 
 int main () {
-
-	Clock test;
-
+	std::cout << "### TEST CONSTRUCTOR VECTOR ###" << std::endl;
 	{
+		Clock test;
+		for (int i = 0; i < 100000; i++) {
+			NAMESPACE::vector<std::string> v;
+			std::cerr << i/1000 << "." << i%1000 << "%" << std::flush << '\r';
+		}
+		test.p();
+	}
+	{
+		Clock test;
 		for (int i = 0; i < 1000; i++) {
 			NAMESPACE::vector<int> v1(100000, 1);
 			NAMESPACE::vector<int> v2(100000, 2);
@@ -67,13 +75,40 @@ int main () {
 			NAMESPACE::vector<int> v7(100000, 7);
 			NAMESPACE::vector<int> v8(100000, 8);
 			NAMESPACE::vector<int> v9(100000, 9);
-			std::cout << i/10 << "." << i%10 << "%" << std::flush << '\r';
+			std::cerr << i/10 << "." << i%10 << "%" << std::flush << '\r';
 			std::cout.clear();
 		}
+		test.p();
 	}
-	long	tmp = test.time();
+	{
+		std::string lst[1000];
+		for (int i = 0; i < 1000; i++)
+			lst[i] = "42";
+		NAMESPACE::vector<std::string> vec(1000, "foo");
+		Clock test;
+		for (int i = 0; i < 10000; i++) {
+			NAMESPACE::vector<std::string> v1(lst, lst + 1000);
+			NAMESPACE::vector<std::string> v2(lst, lst + 950);
+			NAMESPACE::vector<std::string> v4(vec.begin(), vec.begin() + 900);
+			NAMESPACE::vector<std::string> v5(lst, lst + 900);
+			NAMESPACE::vector<std::string> v7(lst, lst + 500);
+			NAMESPACE::vector<std::string> v8(lst, lst + 1000);
+			NAMESPACE::vector<std::string> v9(vec.begin(), vec.end());
 
-	std::cout << tmp/1000 << "." << tmp%1000 << "s" << std::endl;
+			std::cerr << i/100 << "." << i%100 << "%" << std::flush << '\r';
+			std::cout.clear();
+		}
+		test.p();
+	}
+	{
+		Clock test;
+
+		NAMESPACE::vector<std::string> v1(500, "bar");
+		NAMESPACE::vector<NAMESPACE::vector<std::string> >v2 (2, v1);
+		NAMESPACE::vector<NAMESPACE::vector<NAMESPACE::vector<std::string> > > v3(2, v2);
+
+		test.p();
+	}
 /*
 	{
 		ft::vector<int> v;
